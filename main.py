92f0727f84efaf90109cf0a540638abecc4e3449
Felix Riiga <riiga_92@hotmail.com>
2024-09-09 22:59:32 +0200

    Now produces a file

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import sys
+import argparse
 
 from vgmm import vgmm
 
 # Main call
 if __name__ == '__main__':
-    sys.exit(vgmm(sys.argv))
+    argument_parser = argparse.ArgumentParser(prog = 'VGMM',
+                                              description = 'Vägmärkesmakaren - makes signs',
+                                              epilog = 'Have fun!')
+    argument_parser.add_argument('input_file', metavar = 'IN', help = 'The path and file name of the input XML file')
+    argument_parser.add_argument('output_file', metavar = 'OUT', help = 'The path and file name of the output SVG file')
+    argument_parser.add_argument('-v', '--verbose', action = 'store_true', help = 'Be verbose (print debugging info)')
+    args = argument_parser.parse_args()
+
+    # Call to vgmm with parsed arguments, returns exit status when done
+    sys.exit(vgmm(args))
--- vgmm.py
+++ vgmm.py
@@ -1,66 +1,69 @@
 import xml.etree.ElementTree as Et
 import svgwrite
-#import sign as sign
+from svgwrite import mm
+# import sign as sign
 
 
-def vgmm(argv):
-    # Argument checking
-    if len(argv) != 3:
-        return -1
-
-    # Save arguments
-    input_filename = argv[1]
-    output_filename = argv[2]
-
+def vgmm(args):
     # Parse input file from XML
-    root = parse_file(input_filename)
+    root = parse_file(args.input_file)
 
     # Traverse XML tree and make appropriate calls to SVG library
     if root is not None:
+        # Create the sign
         sign_assembly = make_sign(root)
-        # sign_assembly = sign.make_sign(root)
-        sign_assembly.save(indent = True, pretty = True)
 
-    # Produce an SVG file (write to file)
-    # try:
-    #     write_sign(sign_assembly, output_file_name)
-    # except Exception as error:
-    #     print(error)
+        # Save the sign
+        sign_assembly.saveas(args.output_file, indent = True, pretty = True)
 
 
 def make_sign(root):
-    #print(root.iter())
-    sign = svgwrite.Drawing("out.svg", profile = "full")
+    # print(root.iter())
+    sign = svgwrite.Drawing(profile = "full")
 
     for node in root.iter():
         if node.tag == "sign":
             process_sign(sign, node)
-        if node.tag == "ystack":
-            sign.add(process_ystack(node))
-        elif node.tag == "xstack":
-            sign.add(process_xstack(node))
-        elif node.tag == "text":
-            sign.add(process_text(node))
-        elif node.tag == "arrow":
-            sign.add(process_arrow(node))
+        # if node.tag == "ystack":
+        #     sign.add(process_ystack(node))
+        # elif node.tag == "xstack":
+        #     sign.add(process_xstack(node))
+        # elif node.tag == "text":
+        #     sign.add(process_text(node))
+        # elif node.tag == "arrow":
+        #     sign.add(process_arrow(node))
 
     # sign.add(sign.rect((0, 0), (300, 75), fill = "#00755c"))
     # sign.add(sign.rect((1.5, 1.5), (300, 75), 8, 8, fill_opacity = 0.0, stroke = "white", stroke_width = 3))
 
+    total_width = 800
+    total_height = 600
+    sign.viewbox(0, 0, total_width, total_height)
+
     return sign
 
-def process_sign(sign, node):
 
+def process_sign(sign, node):
+    sign.add(get_plate(sign, (300, 75), get_color("green")))
+    sign.add(sign.rect((1.5, 1.5), (300-3, 75-3), 8, 8, fill_opacity = 0.0, stroke = "white", stroke_width = 3))
+
+
+def get_plate(sign, size, color, pos = (0, 0)):
+    return sign.rect(pos, size, fill = color)
+
+
+def get_color(color):
+    if color == "green":
+        return "#00755c"
+
+    return color
 
 # def plate_with_stroke(pos = (0, 0), size, fill_color, stroke_color = "white", stroke_width):
-#
 #     return sign.rect(pos, size, a, b, fill_opacity = 0.0, stroke = stroke_color, stroke_width = stroke_width)
 
 # Feature to select file
 # FIXME
 
-# Help function (print help info)
-# FIXME
 
 # Parse file and return XML tree
 def parse_file(filename):
@@ -76,11 +79,3 @@ def parse_file(filename):
 
 #        for child in root.iter():
 #            print(child.tag, child.attrib)
-
-# Write sign to disk
-def write_sign(sign, filename):
-    try:
-        with open(filename, "w", encoding="utf-8") as output_file:
-            output_file.write(sign)
-    except Exception as error:
-        print(error)
